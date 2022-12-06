b2af5083f923131dc4824e6372a409a5284a5ff5
cezaris13 <pijus.cezar13@gmail.com>
2022-12-06 08:28:49 +0200

    optimized code of day 6

--- AdventOfCode2022/Day6/day6.hs
+++ AdventOfCode2022/Day6/day6.hs
@@ -1,43 +1,24 @@
-import Data.List.Split
 import Data.List
 import System.IO
-import Control.Monad
-import Data.Tuple.Select
-
-type VisitedHouses = [(X,Y,Count)]
-type CurrCoordinate = (X,Y)
-type X = Int
-type Y = Int
-type Count = Int
 
 main :: IO()
 main = do
   file <- openFile "input" ReadMode
   contents <- hGetContents file
-  let oneLine = splitByNewLine contents !! 0
-  print $ part1 oneLine
-  print $ part2 oneLine
+  print $ solution contents 4 -- part 1
+  print $ solution contents 14 -- part 2
 
-splitByNewLine :: String -> [String]
-splitByNewLine = splitOn "\n"
-
-part1 :: String -> Int
-part1 input = part1' str last4 4
+solution :: String -> Int -> Int
+solution input windowSize = solution' str last4 windowSize
   where
-    str = drop 4 input
-    last4 = take 4 input
+    str = drop windowSize input
+    last4 = take windowSize input
 
-part1' :: String -> String -> Int -> Int
-part1' [] _ id = id
-part1' (x:xs) last4 id
+solution' :: String -> String -> Int -> Int
+solution' [] _ id1 = id1
+solution' (x:xs) last4 id
   | isStringFromUniqueChars last4 = id
-  | otherwise = part1' xs (tail last4 ++ [x]) (id+1)
+  | otherwise = solution' xs (tail last4 ++ [x]) (id+1)
 
 isStringFromUniqueChars :: String -> Bool
-isStringFromUniqueChars input = (length $ nub input) == (length input)
-
-part2 :: String -> Int
-part2 input = part1' str last14 14
-  where
-    str = drop 14 input
-    last14 = take 14 input
+isStringFromUniqueChars input = length (nub input) == length input
