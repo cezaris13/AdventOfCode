b6bb34f8d332e3b80c8ab889bd0e52643d790b1d
cezaris13 <pijus.cezar13@gmail.com>
2022-12-09 21:14:07 +0200

    Day 9 part 2 finished in haskell

--- AdventOfCode2022/Day9/day9.hs
+++ AdventOfCode2022/Day9/day9.hs
@@ -1,14 +1,7 @@
 import Data.List.Split
-import Data.List
 import System.IO
-import Control.Monad
-import Data.Maybe
-import Data.Tuple.Select
-import Data.Char
-import GHC.Integer
-import Debug.Trace
 
-type Command = (Char,Int)
+type Command = (String,Int)
 type Coordinate = (Int,Int)
 
 type CoordsVisited = ([Coordinate],[Coordinate])
@@ -29,7 +22,7 @@ splitByNewLine = splitOn "\n"
 parseData :: String -> Command
 parseData input = (c,parsedInt)
   where
-    c = input !! 0
+    c = take 1 input
     parsedInt = parseStringToInt $ drop 2 input
 
 parseStringToInt :: String -> Int
@@ -37,14 +30,14 @@ parseStringToInt = read
 
 moveRope :: CoordsVisited -> Command -> CoordsVisited
 moveRope coords (_,0) = coords
-moveRope coords (command,n) = moveRope updatedCoords (command,n-1)
+moveRope coords (command,n) =moveRope updatedCoords (command,n-1)
   where
     updatedCoords = updateCoordinatesOneCommand coords command
 
-updateCoordinatesOneCommand :: CoordsVisited -> Char -> CoordsVisited
+updateCoordinatesOneCommand :: CoordsVisited -> String -> CoordsVisited
 updateCoordinatesOneCommand coords comm = func' coords comm ([],[])
 
-func' :: CoordsVisited -> Char -> CoordsVisited -> CoordsVisited -- save only last list element positions
+func' :: CoordsVisited -> String -> CoordsVisited -> CoordsVisited -- save only last list element positions
 func' ([],_) _ coords = coords
 func' ([x],visited) command newCoords = (fst newCoords ++ [updatedX], updateVisitedCoords)
   where
@@ -55,7 +48,7 @@ func' ([x],visited) command newCoords = (fst newCoords ++ [updatedX], updateVisi
     updateVisitedCoords
       | elem updatedX (snd newCoords) = snd newCoords
       | otherwise = snd newCoords ++ [updatedX]
-func' (x:xs,vis) command ([],[]) = func' (xs,vis) command ([moveElement x (command,1)],vis)
+func' (x:xs,vis) command ([],[]) =func' (xs,vis) command ([moveElement x (command,1)],vis)
 func' (x:xs,visited) command newCoords = func' (xs,visited) (fst response) (fst newCoords ++ [updatedX],snd newCoords)
   where
     updatedX
@@ -63,28 +56,26 @@ func' (x:xs,visited) command newCoords = func' (xs,visited) (fst response) (fst 
       | otherwise = x
     response =  updateX x (last $ fst newCoords) command
 
-updateX :: Coordinate -> Coordinate -> Char -> (Char,Coordinate)
+updateX :: Coordinate -> Coordinate -> String -> (String,Coordinate)
 updateX (x1,y1) (px2,py2) command
-  | x1-px2 == 0 || y1-py2 == 0 = (command, moveElement (x1,y1) (command,1))
+  | length command == 1 && (x1-px2 == 0 || y1 - py2 == 0) = (command, moveElement (x1,y1) (command,1))
+  | x1-px2 == 0 && length command == 2 = (drop 1 command, moveElement (x1,y1) (drop 1 command,1))
+  | y1-py2 == 0 && length command == 2 = (take 1 command, moveElement (x1,y1) (take 1 command,1))
+  | x1-px2 == 0 && y1-py2 == 0 = (command, (x1,y1) )
   | otherwise = updatedCommand
   where
     updatedCommand
-      | x1 - px2 > 0 && y1 - py2 > 0 = (command,(x1-1,y1-1)) -- these seem to have an effect
-      | x1 - px2 < 0 && y1 - py2 > 0 = (command,(x1+1,y1-1)) -- think of edge cases
-      | x1 - px2 > 0 && y1 - py2 < 0 = (command,(x1-1,y1+1))
-      | x1 - px2 < 0 && y1 - py2 < 0 = (command,(x1+1,y1+1))
+      | x1 - px2 > 0 && y1 - py2 > 0 = ("LD",(x1-1,y1-1)) -- these seem to have an effect
+      | x1 - px2 < 0 && y1 - py2 > 0 = ("RD",(x1+1,y1-1)) -- think of edge cases
+      | x1 - px2 > 0 && y1 - py2 < 0 = ("LU",(x1-1,y1+1))
+      | x1 - px2 < 0 && y1 - py2 < 0 = ("RU",(x1+1,y1+1))
 
 moveElement :: Coordinate -> Command -> Coordinate
 moveElement (x,y) (c,val)
-  | c == 'R' = (x+val,y)
-  | c == 'L' = (x-val,y)
-  | c == 'U' = (x,y+val)
-  | c == 'D' = (x,y-val)
+  | c == "R" = (x+val,y)
+  | c == "L" = (x-val,y)
+  | c == "U" = (x,y+val)
+  | c == "D" = (x,y-val)
 
 chebyshevDistance :: Coordinate -> Coordinate -> Int
 chebyshevDistance (x1,y1) (x2,y2) = max (abs (x1-x2)) (abs (y1-y2))
-
--- 1536 < 2304 < z < 3072
---2592?
-
--- 2688
